041399ab6c79d6cf97ac0924f4b8817b84e0b473
isururanawaka <irjanith@gmail.com>
2014-11-24 17:04:31 +0530

    change method signature

--- modules/integration/tests-integration/tests-transport/src/test/java/org/wso2/carbon/esb/http/inbound/transport/test/HttpInboundTransportTestCase.java
+++ modules/integration/tests-integration/tests-transport/src/test/java/org/wso2/carbon/esb/http/inbound/transport/test/HttpInboundTransportTestCase.java
@@ -23,29 +23,21 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.carbon.integration.common.admin.client.LogViewerClient;
-import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.regex.Matcher;
 
 
 public class HttpInboundTransportTestCase extends ESBIntegrationTest {
-    private ServerConfigurationManager serverConfigurationManager;
-    private LogViewerClient logViewer;
 
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
         super.init();
-        addSequence(getOM("artifacts" + File.separator + "ESB" + File.separator
-                + "http.inbound.transport" + File.separator + "TestIn.xml"));
-        addSequence(getOM("artifacts" + File.separator + "ESB" + File.separator
-                + "http.inbound.transport" + File.separator + "TestOut.xml"));
-        addInboundEndpoint(getOM("artifacts" + File.separator + "ESB" + File.separator
-                + "http.inbound.transport" + File.separator + "synapse.xml"));
+        addSequence(getArtifactConfig("TestIn.xml"));
+        addSequence(getArtifactConfig("TestOut.xml"));
+        addInboundEndpoint(getArtifactConfig("synapse.xml"));
     }
 
     @Test(groups = "wso2.esb", description = "Inbound Http  test case")
@@ -54,8 +46,8 @@ public class HttpInboundTransportTestCase extends ESBIntegrationTest {
             OMElement response = axis2Client.sendSimpleStockQuoteRequest("http://localhost:8081/services/StockQuote", null, "IBM");
             Assert.assertNotNull(response);
             Assert.assertEquals("getQuoteResponse", response.getLocalName());
-        } catch (AxisFault expected) {
-            throw new AxisFault("AxisFault occurred when sending SimpleStockQuoteService", expected);
+        } catch (AxisFault axisFault) {
+            throw new AxisFault("AxisFault occurred when sending SimpleStockQuoteService", axisFault);
         }
     }
 
@@ -65,11 +57,12 @@ public class HttpInboundTransportTestCase extends ESBIntegrationTest {
     }
 
 
-    private OMElement getOM(String relativeFilePath) throws Exception {
+    private OMElement getArtifactConfig(String fileName) throws Exception {
         OMElement synapseConfig = null;
-        relativeFilePath = relativeFilePath.replaceAll("[\\\\/]", Matcher.quoteReplacement(File.separator));
+        String path = "artifacts" + File.separator + "ESB" + File.separator
+                + "http.inbound.transport" + File.separator + fileName;
         try {
-            synapseConfig = esbUtils.loadResource(relativeFilePath);
+            synapseConfig = esbUtils.loadResource(path);
         } catch (FileNotFoundException e) {
             throw new Exception("File Location may be incorrect", e);
         } catch (XMLStreamException e) {
