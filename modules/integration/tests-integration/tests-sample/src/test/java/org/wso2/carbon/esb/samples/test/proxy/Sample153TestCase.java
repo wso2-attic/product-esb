4c7329041bf03bf9293beaceae3f06892b8587f6
warunap <warunap@wso2.com>
2014-11-24 12:05:30 +0530

    fixed

--- modules/integration/tests-integration/tests-sample/src/test/java/org/wso2/carbon/esb/samples/test/proxy/Sample153TestCase.java
+++ modules/integration/tests-integration/tests-sample/src/test/java/org/wso2/carbon/esb/samples/test/proxy/Sample153TestCase.java
@@ -46,7 +46,7 @@ public class Sample153TestCase extends ESBIntegrationTest {
     @Test(groups = "wso2.esb", description = "Custom sequences and endpoints with proxy services")
     public void sample153Test() throws Exception {
         String policyPath = FrameworkPathUtil.getSystemResourceLocation() + "security"+ File.separator +"policies"+ File.separator +"scenario27-policy.xml";
-        OMElement response = new SecureStockQuoteClient().sendSecuredSimpleStockQuoteRequest(null, null, getProxyServiceURLHttps("StockQuoteProxy"),
+        OMElement response = new SecureStockQuoteClient().sendSecuredSimpleStockQuoteRequest(null, null, getProxyServiceURLHttp("StockQuoteProxy"),
                                                                                              policyPath, "Secured");
 
         String lastPrice = response.getFirstElement().getFirstChildWithName(new QName("http://services.samples/xsd", "last"))
