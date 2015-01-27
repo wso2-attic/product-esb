902d6367ae464b5c0d85c1fd2b227da61e562a55
Buddhima <buddhimawijeweera@yahoo.com>
2015-01-27 11:00:30 +0530

    Adding test cases implementation

--- modules/integration/tests-integration/tests-mediator-1/src/test/java/org/wso2/carbon/esb/message/store/jdbc/test/JDBCMessageProcessorTestCase.java
+++ modules/integration/tests-integration/tests-mediator-1/src/test/java/org/wso2/carbon/esb/message/store/jdbc/test/JDBCMessageProcessorTestCase.java
@@ -17,5 +17,78 @@
  */
 package org.wso2.carbon.esb.message.store.jdbc.test;
 
-public class JDBCMessageProcessorTestCase {
+import org.apache.axiom.om.OMElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.test.utils.dbutils.H2DataBaseManager;
+import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
+import org.wso2.esb.integration.common.utils.Utils;
+import org.wso2.esb.integration.common.utils.clients.axis2client.AxisServiceClient;
+
+import java.sql.ResultSet;
+
+public class JDBCMessageProcessorTestCase extends ESBIntegrationTest {
+
+    private H2DataBaseManager h2;
+
+    @BeforeClass(alwaysRun = true)
+    protected void init() throws Exception {
+        super.init();
+        OMElement synapse = esbUtils.loadResource("/artifacts/ESB/jdbc/jdbc_message_store_and_processor_service.xml");
+
+        h2 = new H2DataBaseManager("jdbc:h2:repository/database/WSO2CARBON_DB", "wso2carbon", "wso2carbon");
+
+        h2.execute("CREATE TABLE 'jdbc_store_table' (\n" +
+                   "'indexId' BIGINT( 20 ) NOT NULL ,\n" +
+                   "'msg_id' VARCHAR( 200 ) NOT NULL ,\n" +
+                   "'message' BLOB NOT NULL ,\n" +
+                   "PRIMARY KEY ( 'indexId' )\n" +
+                   ")");
+
+        updateESBConfiguration(synapse);
+    }
+
+    @Test(groups = {"wso2.esb"}, description = "Test proxy service with jdbc message store")
+    public void testJDBCMessageStoreAndProcessor() throws Exception {
+
+        AxisServiceClient client = new AxisServiceClient();
+        for (int i = 0; i < 5; i++) {
+            client.sendRobust(Utils.getStockQuoteRequest("JDBC"), getProxyServiceURLHttp("JDBCStoreAndProcessorTestCaseProxy"), "getQuote");
+        }
+
+        Thread.sleep(10000);
+
+        try {
+
+            ResultSet results = h2.executeQuery("SELECT * FROM jdbc_store_table");
+
+            if (results.getFetchSize() <= 5) {
+
+            } else {
+
+            }
+
+//            for (int i = 0; i < 10; i++) {
+//                if (i < 5) {
+//                    //first 5 messages should be in the queue
+//                    Assert.assertNotNull(consumer.popMessage(), "JMS Message Processor not send message to endpoint");
+//
+//                } else {
+//                    //after 5 no messages should be in the queue
+//                    Assert.assertNull(consumer.popMessage(), "JMS Message Processor sends same message more than once ");
+//                }
+//            }
+        } finally {
+        }
+    }
+
+
+    @AfterClass(alwaysRun = true)
+    public void destroy() throws Exception {
+
+        h2.executeUpdate("DROP TABLE jdbc_store_table");
+
+        super.cleanup();
+    }
 }
--- modules/integration/tests-integration/tests-mediator-1/src/test/java/org/wso2/carbon/esb/message/store/jdbc/test/JDBCMessageStoreProcRESTTestCase.java
+++ modules/integration/tests-integration/tests-mediator-1/src/test/java/org/wso2/carbon/esb/message/store/jdbc/test/JDBCMessageStoreProcRESTTestCase.java
@@ -17,5 +17,89 @@
  */
 package org.wso2.carbon.esb.message.store.jdbc.test;
 
-public class JDBCMessageStoreProcRESTTestCase {
+import org.apache.axiom.om.OMElement;
+import org.apache.http.HttpResponse;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.extensions.servers.httpserver.SimpleHttpClient;
+import org.wso2.carbon.automation.test.utils.dbutils.H2DataBaseManager;
+import org.wso2.carbon.integration.common.admin.client.LogViewerClient;
+import org.wso2.carbon.logging.view.stub.types.carbon.LogEvent;
+import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.testng.Assert.assertEquals;
+
+public class JDBCMessageStoreProcRESTTestCase extends ESBIntegrationTest{
+    private static final String url = "http://localhost:8280/jdbc/store";
+    private static final String logLine0 =
+            "MESSAGE = ************RESTProxy IN, IN-Content-Type = application/json, IN-Test-Header-Field = TestHeaderValue";
+    private static final String logLine1 =
+            "MESSAGE = ************SamplingSeq IN, IN-Content-Type = application/json, IN-Test-Header-Field = TestHeaderValue";
+
+    private final SimpleHttpClient httpClient = new SimpleHttpClient();
+    private final Map<String, String> headers = new HashMap<String, String>(1);
+    private final String payload =  "{\n" +
+                                    "  \"email\" : \"jms@yomail.com\",\n" +
+                                    "  \"firstName\" : \"Jms\",\n" +
+                                    "  \"lastName\" : \"Broker\",\n" +
+                                    "  \"id\" : 10\n" +
+                                    "}";
+
+    private LogViewerClient logViewer;
+
+    private H2DataBaseManager h2;
+
+    @BeforeClass(alwaysRun = true)
+    protected void init() throws Exception {
+        super.init();
+        headers.put("Test-Header-Field", "TestHeaderValue");
+
+        OMElement synapse = esbUtils.loadResource("/artifacts/ESB/jdbc/JDBCMessageStoreREST.xml");
+
+        h2 = new H2DataBaseManager("jdbc:h2:repository/database/WSO2CARBON_DB", "wso2carbon", "wso2carbon");
+
+        h2.execute("CREATE TABLE 'jdbc_store_table' (\n" +
+                   "'indexId' BIGINT( 20 ) NOT NULL ,\n" +
+                   "'msg_id' VARCHAR( 200 ) NOT NULL ,\n" +
+                   "'message' BLOB NOT NULL ,\n" +
+                   "PRIMARY KEY ( 'indexId' )\n" +
+                   ")");
+
+        updateESBConfiguration(synapse);
+        Thread.sleep(1000);
+        logViewer = new LogViewerClient(contextUrls.getBackEndUrl(),getSessionCookie());
+    }
+
+    @Test(groups = {"wso2.esb"}, description = "JDBC Message store support for RESTful services.")
+    public void testJMSMessageStoreAndProcessor() throws Exception {
+        HttpResponse response = httpClient.doPost(url, headers, payload, "application/json");
+        Thread.sleep(10000);
+        assertEquals(response.getStatusLine().getStatusCode(), 202);
+        LogEvent[] logs = logViewer.getAllSystemLogs();
+        int i = 1;
+        for (LogEvent log : logs) {
+            if (log.getMessage().contains(logLine0)) {
+                ++i;
+            }
+            if (log.getMessage().contains(logLine1)) {
+                ++i;
+            }
+        }
+        if (i == 3) {
+            Assert.assertTrue(true);
+        } else {
+            Assert.assertTrue(false);
+        }
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void destroy() throws Exception {
+        h2.executeUpdate("DROP TABLE jdbc_store_table");
+        super.cleanup();
+    }
 }
