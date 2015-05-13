b5d1713f7e5c2cb72fb2d828f6bdd9a781cf67a5
RaivU <ravi.undupitiya@gmail.com>
2015-05-13 13:17:56 +0530

    Finalised HL7 inbound integration tests

--- modules/integration/tests-integration/tests-transport/src/test/java/org/wso2/carbon/esb/hl7/inbound.transport.test/HL7InboundTestSender.java
+++ modules/integration/tests-integration/tests-transport/src/test/java/org/wso2/carbon/esb/hl7/inbound.transport.test/HL7InboundTestSender.java
@@ -17,59 +17,62 @@
  */
 package org.wso2.carbon.esb.hl7.inbound.transport.test;
 
+import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.app.Connection;
-import ca.uhn.hl7v2.app.ConnectionHub;
 import ca.uhn.hl7v2.app.Initiator;
 import ca.uhn.hl7v2.llp.LLPException;
-import ca.uhn.hl7v2.llp.MinLowerLayerProtocol;
 import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.model.v26.message.ADT_A01;
 import ca.uhn.hl7v2.parser.PipeParser;
+import ca.uhn.hl7v2.util.Terser;
 
 import java.io.IOException;
 
 public class HL7InboundTestSender {
 
-	public String send(String host, int port) throws HL7Exception {
-		System.out.println("[ Executing HL7Sender : HOST:" + host + "  ;port :" + port + " ]");
-		// The connection hub connects to listening servers
-		ConnectionHub connectionHub = ConnectionHub.getInstance();
-		// A connection object represents a socket attached to an HL7 server
-		Connection connection =
-		                        connectionHub.attach(host, port, new PipeParser(),
-		                                             MinLowerLayerProtocol.class);
+    private static String OULMessage = "MSH|^~\\&|ABCDEF|ABCDEFGH|WSO2HL7TEST|ABCDEFG|20150112100451.0880+0100||OUL^R22^OUL_R22|HL7Abc000BBB12345D22|P|2.5||||||8859/1\n" +
+            "PID|1||AAAAAA^^^AA^NNBFH~1111^^^ABC^DI^BKK~00280168^^^LAB^LAB~48P00480513^^^CS^SS~80380000100024916352^^^100^HC||XXXXXX^XXXX||19311126|F|||XXX XXX XXX^^Jambugasmulla^^10250^^H^^078332~XXX XXX XXX^^Nugegoda^^13847^^L^^096074~^^^^^^B^^002155||015 706187^ORN^PH^^^^^^^^^015 706187~LANKA 9988^ATT^LK^^^^^^^^^LANKA 9988|015 706187||||AAAAAA|99A12345678||||002155|||100\n" +
+            "PV1|1|E|1640101^^^^^^^^BARRACUDA VALKYRIE||||||||||||||||15100225|||||||||||||||||||||||||20150109\n" +
+            "SPM|1|00107616201||GLO^SOTON GOLD|||||||||||||20150112080000";
 
-		// The initiator is used to transmit unsolicited messages
-		Initiator initiator = connection.getInitiator();
-		ADT_A01 adt = new ADT_A01();
-		try {
-	        adt.initQuickstart("ADT", "A01", "T");
-        } catch (IOException e1) {
-	        // TODO Auto-generated catch block
-	        e1.printStackTrace();
-        }
-	
-		String responseString = null;
+	public String send(String host, int port) throws HL7Exception, IOException, LLPException {
+        HapiContext context = new DefaultHapiContext();
+        Connection c = context.newClient(host, port, false);
+        Initiator initiator = c.getInitiator();
 
-		// send
-		Message response = null;
-		try {
-			response = initiator.sendAndReceive(adt);
-            System.out.println("ENCODE: " + response.encode());
-			PipeParser parser = new PipeParser();
-			responseString = parser.encode(response);
-			System.out.println("Received response:\n" + responseString);
-		} catch (LLPException e) {
-			System.out.println("Error : " + e);
-		} catch (IOException e) {
-			System.out.println("Error : " + e);
-		}
-
-		// Close the connection and server
-		connectionHub.discard(connection);
-
-		return responseString;
+        ADT_A01 msg = new ADT_A01();
+        msg.initQuickstart("ADT", "A01", "T");
+        Message resp = initiator.sendAndReceive(msg);
+        return resp.encode();
 	}
 
+    public String send(String host, int port, Message message) throws HL7Exception, IOException, LLPException {
+        HapiContext context = new DefaultHapiContext();
+        Connection c = context.newClient(host, port, false);
+        Initiator initiator = c.getInitiator();
+
+        Message resp = initiator.sendAndReceive(message);
+        return resp.encode();
+    }
+
+    public Message getOULMessage() throws HL7Exception {
+        PipeParser parser = new PipeParser();
+        return parser.parse(OULMessage);
+    }
+
+    public static void main (String[] args) {
+        HL7InboundTestSender sender = new HL7InboundTestSender();
+        try {
+            String resp = sender.send("localhost", 20001, sender.getOULMessage());
+            System.out.println(resp);
+        } catch (HL7Exception e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LLPException e) {
+            e.printStackTrace();
+        }
+    }
 }
