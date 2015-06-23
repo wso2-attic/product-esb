d3cadea4bdf1e89fa038c8d7038940e8e9ef8d30
Maheeka Jayasuriya <maheeka@wso2.com>
2015-06-23 15:35:49 +0530

    add rabbitmq large message consumer/producer test cases

--- modules/integration/tests-integration/tests-platform/tests-rabbitmq/src/test/java/org/wso2/carbon/esb/rabbitmq/transport/test/RabbitMQConsumerTestCase.java
+++ modules/integration/tests-integration/tests-platform/tests-rabbitmq/src/test/java/org/wso2/carbon/esb/rabbitmq/transport/test/RabbitMQConsumerTestCase.java
@@ -25,6 +25,7 @@ import org.wso2.carbon.integration.common.admin.client.LogViewerClient;
 import org.wso2.carbon.logging.view.stub.types.carbon.LogEvent;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
 import org.wso2.esb.integration.common.utils.clients.rabbitmqclient.RabbitMQProducerClient;
+import org.wso2.esb.integration.common.utils.common.FixedSizeSymbolGenerator;
 
 import java.io.IOException;
 
@@ -47,15 +48,49 @@ public class RabbitMQConsumerTestCase extends ESBIntegrationTest {
 
         try {
             sender.declareAndConnect("exchange2", "queue2");
+            String message =
+                    "<ser:placeOrder xmlns:ser=\"http://services.samples\">\n" +
+                            "<ser:order>\n" +
+                            "<ser:price>100</ser:price>\n" +
+                            "<ser:quantity>2000</ser:quantity>\n" +
+                            "<ser:symbol>RMQ</ser:symbol>\n" +
+                            "</ser:order>\n" +
+                            "</ser:placeOrder>";
             for (int i = 0; i < 200; i++) {
-                String message =
-                        "<ser:placeOrder xmlns:ser=\"http://services.samples\">\n" +
-                                "<ser:order>\n" +
-                                "<ser:price>100</ser:price>\n" +
-                                "<ser:quantity>2000</ser:quantity>\n" +
-                                "<ser:symbol>RMQ</ser:symbol>\n" +
-                                "</ser:order>\n" +
-                                "</ser:placeOrder>";
+                sender.sendBasicMessage(message);
+            }
+        } catch (IOException e) {
+            Assert.fail("Could not connect to RabbitMQ broker");
+        } finally {
+            sender.disconnect();
+        }
+
+        Thread.sleep(20000);
+
+        LogEvent[] logs = logViewer.getAllRemoteSystemLogs();
+        int afterLogSize = logs.length;
+        int count = 0;
+
+        for (int i = (afterLogSize - beforeLogSize - 1); i >= 0; i--) {
+            String message = logs[i].getMessage();
+            if (message.contains("received = true")) {
+                count++;
+            }
+        }
+
+        Assert.assertEquals(count, 200, "All messages are not received from queue");
+    }
+
+    @Test(groups = {"wso2.esb"}, description = "Test ESB as a RabbitMQ Consumer with large messages ~10KB")
+    public void testRabbitMQConsumerLargeMessage() throws Exception {
+        int beforeLogSize = logViewer.getAllRemoteSystemLogs().length;
+
+        RabbitMQProducerClient sender = new RabbitMQProducerClient("localhost", 5672, "guest", "guest");
+
+        try {
+            sender.declareAndConnect("exchange2", "queue2");
+            String message = FixedSizeSymbolGenerator.generateMessageKB(10);
+            for (int i = 0; i < 200; i++) {
                 sender.sendBasicMessage(message);
             }
         } catch (IOException e) {
--- modules/integration/tests-integration/tests-platform/tests-rabbitmq/src/test/java/org/wso2/carbon/esb/rabbitmq/transport/test/RabbitMQProducerTestCase.java
+++ modules/integration/tests-integration/tests-platform/tests-rabbitmq/src/test/java/org/wso2/carbon/esb/rabbitmq/transport/test/RabbitMQProducerTestCase.java
@@ -17,6 +17,7 @@
 
 package org.wso2.carbon.esb.rabbitmq.transport.test;
 
+import org.apache.axiom.om.OMElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -71,6 +72,56 @@ public class RabbitMQProducerTestCase extends ESBIntegrationTest {
             }
         }
         consumer.disconnect();
+    }
+
+    @Test(groups = {"wso2.esb"}, description = "Test ESB as a RabbitMQ Consumer with large messages ~10KB ")
+    public void testRabbitMQProducerLargeMessages() throws Exception {
+        AxisServiceClient client = new AxisServiceClient();
+        RabbitMQConsumerClient consumer = new RabbitMQConsumerClient("localhost");
+
+        try {
+            consumer.declareAndConnect("exchange1", "queue1");
+        } catch (IOException e) {
+            Assert.fail("Could not connect to RabbitMQ broker");
+        }
+
+        List<String> messages = consumer.popAllMessages();
+        int beforeMessageCount = messages.size();
+        String symbol = getFixedSizeSymbolKB("RMQ", 10);
+        OMElement payload = Utils.getStockQuoteRequest(symbol);
+
+        for (int i = 0; i < 5; i++) {
+            client.sendRobust(payload, getProxyServiceURLHttp("RabbitMQProducerProxy"), "getQuote");
+        }
+        Thread.sleep(10000);
+
+        messages = consumer.popAllMessages();
+        int afterMessagesCount = messages.size();
+
+        if (messages.size() == 0) {
+            Assert.fail("Messages not received at RabbitMQ Broker");
+        } else {
+            Assert.assertEquals(afterMessagesCount - beforeMessageCount, 5, "Messages not received at RabbitMQBroker");
+            for (int i = beforeMessageCount; i < afterMessagesCount; i++) {
+                Assert.assertNotNull(messages.get(i), "Message not found. message sent by proxy service not reached to the destination Queue");
+                Assert.assertTrue(messages.get(i).contains("<ns:getQuote xmlns:ns=\"http://services.samples\"><" +
+                        "ns:request><ns:symbol>" + symbol + "</ns:symbol></ns:request></ns:getQuote>")
+                        , "Message mismatched");
+            }
+        }
+        consumer.disconnect();
+    }
+
+    private String getFixedSizeSymbolKB(String symbol, double size) {
+        StringBuilder sb = new StringBuilder();
+        String message = symbol;
+        int length = message.length();
+        double bytes = size * 1024;
+        sb.append(" WSO2 ");
+        for (int i = 0; i < bytes; i += length) {
+            sb.append(message);
+        }
+        return sb.toString();
     }
 
     @AfterClass(alwaysRun = true)
