de17ecf740c4bb6dcaa0727e7647cb7e08584f60
jagatha <jagatha@wso2.com>
2015-08-04 16:55:21 +0530

    Sample16TestCase enabled and corrected the issue of registry not updating with ESBRegistry.

--- modules/integration/tests-integration/tests-sample/src/test/java/org/wso2/carbon/esb/samples/test/mediation/Sample16TestCase.java
+++ modules/integration/tests-integration/tests-sample/src/test/java/org/wso2/carbon/esb/samples/test/mediation/Sample16TestCase.java
@@ -18,6 +18,7 @@
 package org.wso2.carbon.esb.samples.test.mediation;
 
 import javax.xml.namespace.QName;
+
 import org.apache.axiom.om.OMElement;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
@@ -25,22 +26,42 @@ import static org.testng.Assert.assertTrue;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
+import org.wso2.carbon.base.CarbonBaseUtils;
+import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
 import org.wso2.esb.integration.common.utils.ESBTestConstant;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Sample 16: Introduction to Dynamic and Static Registry Keys
  */
 public class Sample16TestCase extends ESBIntegrationTest {
-    private String oldSynapseConfig;
+    private ServerConfigurationManager serverConfigurationManager;
 
     @BeforeClass(alwaysRun = true)
     public void uploadSynapseConfig() throws Exception {
         super.init();
+        serverConfigurationManager = new ServerConfigurationManager(context);
+
+        //Update registry.xml with ESBRegistry - this is required because test framework does not update registry from
+        //config file when using loadSampleESBConfiguration() method
+        File sourceFile = new File(FrameworkPathUtil.getSystemResourceLocation() + File.separator + "artifacts"
+                                   + File.separator + "ESB" + File.separator + "samples" + File.separator + "sample16"
+                                   + File.separator + "registry.xml");
+        File targetFile = new File(CarbonBaseUtils.getCarbonHome() + File.separator + "repository"
+                                   + File.separator + "deployment" + File.separator + "server" + File.separator
+                                   + "synapse-configs" + File.separator + "default" + File.separator + "registry.xml");
+        serverConfigurationManager.applyConfigurationWithoutRestart(sourceFile, targetFile, true);
+        serverConfigurationManager.restartGracefully();
+        TimeUnit.SECONDS.sleep(10);
+        super.init();
         loadSampleESBConfiguration(16);
     }
 
-    @Test(groups = { "wso2.esb" }, description = "Dynamic and Static Registry Keys", enabled = false)
+    @Test(groups = {"wso2.esb"}, description = "Dynamic and Static Registry Keys")
     public void testDynamicAndStaticRegistryKeys() throws Exception {
         OMElement response = axis2Client.sendCustomQuoteRequest(getMainSequenceURL()
                 , getBackEndServiceUrl(ESBTestConstant.SIMPLE_STOCK_QUOTE_SERVICE), "WSO2");
@@ -48,14 +69,17 @@ public class Sample16TestCase extends ESBIntegrationTest {
         assertEquals(response.getLocalName(), "CheckPriceResponse", "CheckPriceResponse not match");
         assertTrue(response.toString().contains("Price"), "No price tag in response");
         assertTrue(response.toString().contains("Code"), "No code tag in response");
-        assertEquals(response.getFirstChildWithName
-                         (new QName("http://services.samples/xsd", "Code")).getText(), "WSO2",
-                     "Symbol not match"
-        );
+        assertEquals(response.getFirstChildWithName (new QName("http://services.samples/xsd", "Code")).getText(),
+                     "WSO2", "Symbol not match");
     }
 
     @AfterClass(alwaysRun = true)
     public void destroy() throws Exception {
-        super.cleanup();
+        try {
+            super.cleanup();
+        } finally {
+            serverConfigurationManager.restoreToLastConfiguration(false);
+            serverConfigurationManager = null;
+        }
     }
 }
